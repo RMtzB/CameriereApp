ac1b9a96fc7152a487fd7bb987e4b84ff37b535c
RMtzB <wdspectra@gmail.com>
2020-11-03 19:58:05 -0600

    Se esta modificando el metodo getRestaurantMenu

--- app/src/main/java/app/proyecto/Utils/BusinessLogic.java
+++ app/src/main/java/app/proyecto/Utils/BusinessLogic.java
@@ -1,6 +1,8 @@
 package app.proyecto.Utils;
 
 import android.content.Context;
+
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -19,28 +21,13 @@ public class BusinessLogic {
 	}
 
 	public static List<Product> getRestaurantMenu(String restaurantIdentifier) {
-//		FirebaseFirestore firebaseFirestore = FirebaseFirestore.getInstance();
-//		String nameRestaurant = "";
+		FirebaseFirestore firebaseFirestore = FirebaseFirestore.getInstance();
 		List<Product> items = new ArrayList<>();
-//
-//
-//		firebaseFirestore.collection(nameRestaurant).get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
-//			@Override
-//			public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
-//				if(!queryDocumentSnapshots.isEmpty()) {
-//					List<DocumentSnapshot> documentos = queryDocumentSnapshots.getDocuments();
-//
-//					for (int i = 0; i < documentos.size(); i++) {
-//						DocumentSnapshot document = documentos.get(i);
-//
-//						items.add(new Product(document.getString("Imagen"), document.getString("Nombre"), document.getString("Descripcion"), document.getDouble("Precio"), document.getString("Ingredientes")));
-//					}
-//				}
-//			}
-//		});
 
-		items.add(new Product(R.drawable.img01 + "", "Nombre del producto", "Esta es la descripcion del producto", 34.00, "dsdfsd, dfsdf, dsfdsfs, dsfsdfsdfsdfsd, dsf"));
-		items.add(new Product(R.drawable.img02 + "", "Nombre del producto", "Esta es la descripcion del producto", 34.00, "dsdfsd, dfsdf, dsfdsfs, dsfsdfsdfsdfsd, dsf"));
+		firebaseFirestore.collection(restaurantIdentifier).get().addOnCanceledListener(new OnCompleteListener<Task>());
+
+//		items.add(new Product(R.drawable.img01 + "", "Nombre del producto", "Esta es la descripcion del producto", 34.00, "dsdfsd, dfsdf, dsfdsfs, dsfsdfsdfsdfsd, dsf"));
+//		items.add(new Product(R.drawable.img02 + "", "Nombre del producto", "Esta es la descripcion del producto", 34.00, "dsdfsd, dfsdf, dsfdsfs, dsfsdfsdfsdfsd, dsf"));
 
 		return items;
 	}
